c274adae551bef124f8806717e550d9bef090089
Christopher Fox <cfox1290@gmail.com>
2018-05-20 13:19:38 -0600

    basic layout started

--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-class App extends React.Component{
-    render(){
-        return(
-            <h1>MP3 Player</h1>
+
+class Marquee extends React.Component {
+    render() {
+        return (
+            <section className="music-marquee">
+                <div className="headings">
+                    {this.props.mp3artist} - {this.props.children} ({this.props.genre})
+                </div>
+            </section>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MP3Player extends React.Component {
+    render() {
+        return (
+            <div className="audio-container">
+                <h1>{this.props.title}</h1>
+                <Marquee
+                    mp3artist="A Name"
+                    mp3genre="Some genre">Song</Marquee>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<MP3Player title="MP3 Jukebox..." />, document.getElementById('root'));
 registerServiceWorker();
